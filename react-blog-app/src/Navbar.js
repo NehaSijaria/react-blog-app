a11e2d7fd532a165893f115649eafe68adafee3c
Neha <nehasijaria@gmail.com>
2021-12-09 11:27:02 -0500

    removed dynamic inline styles

--- react-blog-app/src/Navbar.js
+++ react-blog-app/src/Navbar.js
@@ -6,12 +6,7 @@ export const Navbar = () => {
      <h1>My Blog</h1>
      <div className="links">
        <a href="/">Home</a>
-       <a href="/create" style={{
-         color: "white",
-         backgroundColor:"purple",
-         borderRadius:"50px"
-       }}
-       >New blog</a>
+       <a href="/create" >New blog</a>
      </div>
    </nav>
   )
