0b526abd39f01ee376d48f8e91870810be8f649a
Neha <nehasijaria@gmail.com>
2021-12-09 00:45:13 -0500

    update imp notes and clean code

--- react-blog-app/src/App.js
+++ react-blog-app/src/App.js
@@ -3,17 +3,10 @@ import './App.css';
 //jsx code - template with logic
 function App() {
   //any valid js can we written here
-  const title = 'welcome app component';
-  const likes = [50, 100];//return as string
-  const link = "https://google.com"
   return (
     <div className="App">
       <div className="content">
-        <h1>{ title }</h1>
-        <p>liked { likes } </p>
-        <p>{ [1,2,3] }</p>
-        <a href={link}>Check me</a>
-        {/* returned as string */}
+        hello
       </div>
       
     </div>
