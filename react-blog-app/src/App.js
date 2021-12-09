6c0588cb09b6ec299c388271216a5ac351f19a4d
Neha <nehasijaria@gmail.com>
2021-12-09 10:45:37 -0500

    creating navbar

--- react-blog-app/src/App.js
+++ react-blog-app/src/App.js
@@ -1,10 +1,12 @@
-
 import './App.css';
+import { Navbar } from './Navbar';
+
 //jsx code - template with logic
 function App() {
   //any valid js can we written here
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
         hello
       </div>
