e1e1e5ae3f714209395f9f91e40b3e6b1bf3ee97
Neha <nehasijaria@gmail.com>
2021-12-09 11:58:28 -0500

    access args inside fn

--- react-blog-app/src/Home.js
+++ react-blog-app/src/Home.js
@@ -4,11 +4,17 @@ export const Home = () => {
   const handleClick = () => {
     console.log('you clicked me')
   }
+  const handleClickAgain = (name) => {
+    console.log(`you clicked me ${name}`)
+  }
   return (
     <div className="home">
       <h1>Home page</h1>
       {/* function(by ref) handling dynamically */}
       <button onClick={handleClick}>Click Me</button>
+      {/* Cant pass args like this: its invoking which is not acceptable, so use anonymouse fn */}
+      {/* <button onClick={handleClickAgain(2)}>Click Again</button> */}
+      <button onClick={() => handleClickAgain('sam')}>Click Again</button>
     </div>
   )
 }
