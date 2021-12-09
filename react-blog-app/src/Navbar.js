000e8c06a711c99585d1ab6c295156526cf27144
Neha <nehasijaria@gmail.com>
2021-12-09 11:26:15 -0500

    add dynamic inline styles

--- react-blog-app/src/Navbar.js
+++ react-blog-app/src/Navbar.js
@@ -6,7 +6,12 @@ export const Navbar = () => {
      <h1>My Blog</h1>
      <div className="links">
        <a href="/">Home</a>
-       <a href="/create">New blog</a>
+       <a href="/create" style={{
+         color: "white",
+         backgroundColor:"purple",
+         borderRadius:"50px"
+       }}
+       >New blog</a>
      </div>
    </nav>
   )
