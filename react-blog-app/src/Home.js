6feb5ad3066a10d95fa46532bff998f375d21d49
Neha <nehasijaria@gmail.com>
2021-12-09 21:00:21 -0500

    showing list &updated css

--- react-blog-app/src/Home.js
+++ react-blog-app/src/Home.js
@@ -1,9 +1,23 @@
 import { useState } from "react";
 
 export const Home = () => {
+  let blogDesc = [
+    { title: 'Diet Chart', body: 'lorem ipsum...', author: 'sam', id: 1 },
+    { title: 'Healthy Food!', body: 'lorem ipsum...', author: 'John', id: 2 },
+    { title: 'Weekend-Cheat-Foods', body: 'lorem ipsum...', author: 'sam', id: 3 }
+  ]
+  //initial value will be array of blogs
+  const [blogs, setBlogs] = useState(blogDesc)
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      {blogs.map((blog) => (
+        //return jsx template must be inside ()
+        // react will keep track each item thru keyproperty(unique).each root el in map must hv key.
+        <div className="blog-preview" key={blog.id}>
+          <h2>Title: {blog.title}</h2>
+          <p>Author: {blog.author}</p>
+        </div>
+      ))}
     </div>
   )
 }
